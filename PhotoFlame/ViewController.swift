7001158a661d6dd34d4b7033cf59e7e0a7bf2129
SSRtnz <ssrtnzz@gmail.com>
2023-03-13 15:42:29 +0700

    add PhotoFlame.xcodeproj/xcuserdata/ssrtnz.xcuserdatad/xcdebugger/Breakpoints_v2.xcbkptlist

--- PhotoFlame/ViewController.swift
+++ PhotoFlame/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func ButtonOfPrintTest(_ sender: Any) {
+        print("kow pad ka pow")
+    }
 }
 
